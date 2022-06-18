5979c7d93185645363ec3687775a46d2aa54bf01
MehmetBugcrowd <102980873+MehmetBugcrowd@users.noreply.github.com>
2022-06-18 02:13:25 +0200

    Update test.yaml.php

--- test.yaml.php
+++ test.yaml.php
@@ -6,7 +6,7 @@ securityDefinitions:
 info:
   version: "0.0.1"
   title: Example Title
-  description: html inject <img src=image.png>
+  description: html inject <img src=https://mert.ninja/imgs/pp4.jpeg>
 paths:
   /:
     get:
